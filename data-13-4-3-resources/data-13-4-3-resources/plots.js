e3cc6af0370fb353493ae074301ff2293fec092d
Jessica Bauman <jbauman815@gmail.com>
2023-01-16 17:22:41 -0500

    adding lesson 4-3

--- data-13-4-3-resources/data-13-4-3-resources/plots.js
+++ data-13-4-3-resources/data-13-4-3-resources/plots.js
@@ -1,5 +1,5 @@
 // 13.4.3
-// Generate Dropdown Menu Items
+// Creates a dropdown menu of ID numbers dynamically
 function init() {
     var selector = d3.select("#selDataset");
   
@@ -15,3 +15,26 @@ function init() {
   })}
   
   init();
+
+  function optionChanged(newSample) {
+    console.log(newSample);
+  }
+// optionChanged() is called from the HTML document and, in turn, calls buildMetadata() and buildCharts()
+// newSample, is the volunteer ID number that is passed to both of these functions
+  function optionChanged(newSample) {
+    buildMetadata(newSample);
+    buildCharts(newSample);
+  }
+
+// declare the first of these functions: buildMetadata()
+  function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+      var metadata = data.metadata;
+      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+      var result = resultArray[0];
+      var PANEL = d3.select("#sample-metadata");
+  
+      PANEL.html("");
+      PANEL.append("h6").text(result.location);
+    });
+  }
